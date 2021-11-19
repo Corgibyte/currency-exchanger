2dc255833abf1a26b2cb36a720ca309a01125bf9
Hannah Y <hannah@corgibyte.com>
2021-11-19 12:45:09 -0800

    Use sessionStorage for storing exchange data

--- src/index.js
+++ src/index.js
@@ -5,24 +5,27 @@ import './css/styles.css';
 import Exchange from './js/exchange.js';
 import Codes from './assets/data/currencyCodes.json';
 
-let exchange;
 getExchange();
 
 $('#inputForm').on('submit', (event) => {
   event.preventDefault();
+  const exchange = JSON.parse(sessionStorage.getItem('exchange'));
   const targetCurrency = $('#currencies').val();
   const exchangeAmount = parseFloat($('#exchangeAmount').val());
   $('#outputMessage').text(exchange.convert(targetCurrency, exchangeAmount));
 });
 
 function getExchange(originCurrency = "USD") {
-  Exchange.build(originCurrency).then((newExchange) => {
-    exchange = newExchange;
-    getCurrencyElements();
-  });  
+  if (!sessionStorage.getItem('exchange') || !Object.prototype.hasOwnProperty.call(sessionStorage.getItem('exchange'), 'exchangeRate')) {
+    Exchange.build(originCurrency).then((newExchange) => {
+      sessionStorage.setItem('exchange', JSON.stringify(newExchange));
+      getCurrencyElements();
+    });  
+  }
 }
 
 function getCurrencyElements() {
+  const exchange = JSON.parse(sessionStorage.getItem('exchange'));
   const currencies = Object.keys(exchange.exchangeRate);
   let htmlString = "<option selected>Choose a currency</option>";
   currencies.forEach((currency) => {
