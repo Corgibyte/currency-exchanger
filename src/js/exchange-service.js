9ad833f5b1f95697b03e69aa8b8d27e117fe9a51
Hannah Y <hannah@corgibyte.com>
2021-11-19 10:44:54 -0800

    BUGFIX: Fix broken API call

--- src/js/exchange-service.js
+++ src/js/exchange-service.js
@@ -1,14 +1,12 @@
 export default class ExchangeService {
-  static async getExchangeRate(currency = "USD") {
-    try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/{currency}`);
-      if (!response.ok) {
-        throw Error(response.statusText);
+  static getExchangeRate(currency = "USD") {
+    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`;
+    return fetch(url).then((fetchResponse) => {
+      if (!fetchResponse.ok) {
+        throw Error(fetchResponse.statusText);
+      } else {
+        return fetchResponse.json();
       }
-      const data = await response.json();
-      return data;
-    } catch(error) {
-      return error.message;
-    }    
+    });
   }
 }
