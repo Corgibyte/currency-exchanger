4be5f66313a2a2bc59d4e0a381a6bc66ec2bcd28
Hannah Y <hannah@corgibyte.com>
2021-11-19 10:45:10 -0800

    Implement basic UI logic

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import Exchange from './js/exchange.js';
+
+let exchange;
+getExchange();
+
+$('#inputForm').on('submit', (event) => {
+  event.preventDefault();
+  const targetCurrency = $('targetCurrency').val();
+  const exchangeAmount = parseFloat($('exchangeAmount').val());
+  console.log(exchange.convert(targetCurrency, exchangeAmount));
+  //$('outputMessage').text(exchange.convert(targetCurrency, exchangeAmount));
+});
+
+function getExchange(originCurrency = "USD") {
+  Exchange.build(originCurrency).then((newExchange) => {
+    exchange = newExchange;
+  });  
+}
