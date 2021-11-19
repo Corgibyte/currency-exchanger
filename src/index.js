a781a6eea530d3fadac3abaa669ac1e2b2e1870b
Hannah Y <hannah@corgibyte.com>
2021-11-19 11:17:50 -0800

    Implement select box for all currencies provided

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from './js/exchange.js';
+import Codes from './assets/data/currencyCodes.json';
 
 let exchange;
 getExchange();
 
 $('#inputForm').on('submit', (event) => {
   event.preventDefault();
-  const targetCurrency = $('#targetCurrency').val();
+  const targetCurrency = $('#currencies').val();
   const exchangeAmount = parseFloat($('#exchangeAmount').val());
   $('#outputMessage').text(exchange.convert(targetCurrency, exchangeAmount));
 });
@@ -17,5 +18,15 @@ $('#inputForm').on('submit', (event) => {
 function getExchange(originCurrency = "USD") {
   Exchange.build(originCurrency).then((newExchange) => {
     exchange = newExchange;
+    getCurrencyElements();
   });  
+}
+
+function getCurrencyElements() {
+  const currencies = Object.keys(exchange.exchangeRate);
+  let htmlString = "<option selected>Choose a currency</option>";
+  currencies.forEach((currency) => {
+    htmlString += `<option value="${currency}">${currency}: ${Codes[currency]}</option>`;
+  });
+  $('#currencies').html(htmlString);
 }
