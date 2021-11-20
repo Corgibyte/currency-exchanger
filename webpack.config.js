1643caae0c975ae5da1faf39db03cf235d13570b
Hannah Y <hannah@corgibyte.com>
2021-11-19 16:07:10 -0800

    Add favicon and build support

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -22,7 +23,8 @@ module.exports = {
       template: './src/index.html',
       inject: 'body'
     }),
-    new ESLintPlugin()
+    new ESLintPlugin(),
+    new FaviconsWebpackPlugin('./src/assets/img/icon.png')
   ],
   module: {
     rules: [
